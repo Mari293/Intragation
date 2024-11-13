2a547de81d42a586df65c737fd4c1a8c3f56dd13
Mari293 <maricruzg020903@gmail.com>
2024-11-12 21:02:44 -0500

    Pipline for aws integration

---
+++ main.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/saludo/{name}")
+async def say_hello(name:str):
+  return {"Esto es un saludo "}
